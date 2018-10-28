22e1b0e348711385ce459ca5b1389a0beb33b9cd
aqqje <1042136232@qq.com>
2018-10-28 13:23:24 +0800

    过滤条件构建

--- pinyougou-parent/pinyougou-search-web/src/main/webapp/js/controller/searchController.js
+++ pinyougou-parent/pinyougou-search-web/src/main/webapp/js/controller/searchController.js
@@ -1,8 +1,30 @@
 //搜索控制层
 app.controller("searchController",function($scope,searchSerivce){
+	//搜索
 	$scope.search=function(){
 		searchSerivce.search($scope.searchMap).success(function(response){
 			$scope.resultMap = response;
 		});
+	}
+	
+	$scope.searchMap={'keywords':'','category':'','brand':'','spec':{}} // 搜索对象
+	
+	//添加搜索选项
+	$scope.addSearchItem=function(key,keyValue){
+		if(key=='category'||key=='brand'){
+			$scope.searchMap[key]=keyValue;
+		}else{
+			$scope.searchMap.spec[key]=keyValue;
+		}
+		$scope.search();//执行搜索
+	}
+	//移除搜索选项
+	$scope.removeSearchItem=function(key){
+		if(key=='category'||key=='brand'){
+			$scope.searchMap[key]='';
+		}else{
+			delete $scope.searchMap.spec[key];
+		}
+		$scope.search();//执行搜索
 	}
 });
